0796543fe468bd4a4eda03a557cb7a184597880b
Jose Ortiz <71667538+Jortizuwu@users.noreply.github.com>
2024-04-19 15:27:32 -0500

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './core/app'
 
+console.log({
+  env1: import.meta.env.VITE_BASE_URL_API,
+  env2: import.meta.env.BASE_URL_IMAGE,
+});
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
